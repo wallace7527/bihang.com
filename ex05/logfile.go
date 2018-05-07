3b4ce9488394e8dac73f2917ca550c140e9d694b
Wang Lei <wanglei.ok@foxmail.com>
2018-05-07 20:14:23 +0800

    Use WSS to track transaction records in real time and retrieve the latest block data after each successful connection to ensure that data will not be missed during the disconnected period

---
+++ ex05/logfile.go
@@ -0,0 +1,93 @@
+/*
+Copyright 2018 The go-eam Authors
+This file is part of the go-eam library.
+
+logfile
+设置标准日志的输出格式并输出到文件
+
+
+wanglei.ok@foxmail.com
+
+1.0
+版本时间：2018年4月13日18:32:12
+
+*/
+
+package main
+
+import (
+	"os"
+	"io"
+	"path/filepath"
+	"strings"
+	"fmt"
+	"time"
+	"log"
+)
+
+//取得当前可执行程序路径
+func getCurrentDirectory() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.Replace(dir, "\\", "/", -1)
+}
+
+//创建path执行的文件夹
+func createDir(path string) bool {
+	// check
+	if _, err := os.Stat(path); err != nil {
+		//不存在创建
+		err := os.MkdirAll(path, 0711)
+
+		if err != nil {
+			return false
+		}
+	}
+	return true
+}
+
+//取得当前可执行程序名称
+func baseName() string {
+	return filepath.Base(os.Args[0])
+}
+
+//在当前可执行程序路径下创建log文件夹存放日志
+func createLogDir() string {
+	//当前目录加log
+	logPath := fmt.Sprintf("%s/log", getCurrentDirectory())
+	if createDir(logPath) {
+		return logPath
+	}
+	return  ""
+}
+
+//指定路径、前缀、后缀，返回格式化的日志文件路径
+//格式化方式为 /path/to/file/<prefix>YYYYMMDD<suffix>
+func logFilePath(path, prefix, suffix string) string {
+	return fmt.Sprintf("%s/%s%s%s", path, prefix, time.Now().Format("20060102"), suffix)
+}
+
+//设置默认logger的
+//标志位log.Ldate | log.Lmicroseconds
+//设置日志输出到文件 ./log/baseName_YYYYMMDD.log
+func logSetup() {
+	log.SetFlags(log.Ldate | log.Lmicroseconds )
+
+	//创建日志文件夹
+	logDir := createLogDir()
+	if len(logDir) != 0 {
+		//baseName_YYYYMMDD.log
+		filePath := logFilePath(logDir, baseName()+"_",".log")
+		file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			return
+		}
+
+		log.SetOutput(io.MultiWriter(file, os.Stderr))
+
+	}
+}
+
+
--- ex05/main.go
+++ ex05/main.go
@@ -1,20 +1,44 @@
+/*
+Copyright 2018 The go-eam Authors
+This file is part of the go-eam library.
+
+main
+这个例子用以太坊etherscan Websocket接口，用于监控某个账号的交易事件。
+
+
+wanglei.ok@foxmail.com
+
+1.0
+版本时间：2018年4月13日18:32:12
+
+*/
+
 package main
 
 import (
 	"flag"
-	"log"
 	"net/url"
 	"os"
 	"os/signal"
 	"time"
-
+	"log"
 	"github.com/gorilla/websocket"
 	"github.com/pkg/errors"
+	"encoding/json"
+	"bytes"
+	"strings"
+	"strconv"
 )
 
 
-//这个会以太坊etherscan Websocket接口，用于监控某个账号的交易事件。
-var addr = flag.String("addr", "socket.etherscan.io", "http service address")
+const (
+	ETHERSCANAPI_ADDR   = "https://etherscan.io/address/"
+	ETHERSCANAPI_TX     = "https://etherscan.io/tx/"
+)
+
+var (
+	eais map[string]int
+)
 
 type EtherscanWS struct{
 	url url.URL
@@ -26,43 +50,168 @@ func NewEtherscanWS() *EtherscanWS{
 	}
 }
 
+
+//type (
+//	//交易数据对象
+//	EventTxJson struct {
+//		BlockNumber	string	`json:"blockNumber"`
+//		TimeStamp	string	`json:"timeStamp"`
+//		Hash	string	`json:"hash"`
+//		Nonce	string	`json:"nonce"`
+//		BlockHash	string	`json:"blockHash"`
+//		TransactionIndex	string	`json:"transactionIndex"`
+//		From	string	`json:"from"`
+//		To	string	`json:"to"`
+//		Value	string	`json:"value"`
+//		Gas	string	`json:"gas"`
+//		GasPrice	string	`json:"gasPrice"`
+//		Input	string	`json:"input"`
+//		ContractAddress	string	`json:"contractAddress"`
+//		CumulativeGasUsed	string	`json:"cumulativeGasUsed"`
+//		GasUsed	string	`json:"gasUsed"`
+//		Confirmations	string	`json:"confirmations"`
+//		IsError string `json:"isError"`
+//	}
+//
+//	//交易列表对象
+//	EventTxlistJson struct {
+//		Event string  `json:"event"`
+//		Address string  `json:"address"`
+//		Result [] EventTxJson `json:"result"`
+//	}
+//
+//)
+
+func init(){
+	logSetup()
+}
+
 func (e * EtherscanWS)start() error {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	log.Printf("connecting to %s", e.url.String())
+	log.Printf("Connecting to %s", e.url.String())
 
 	//连接
 	c, _, err := websocket.DefaultDialer.Dial(e.url.String(), nil)
 	if err != nil {
-		log.Println("dial:", err)
+		log.Println("Dial:", err)
 		return err
 	}
 	defer c.Close()
 
 	done := make(chan struct{})
 
-	//启动接受历程
+	//启动接收历程
 	go func() {
 		defer close(done)
 		for {
 			_, message, err := c.ReadMessage()
 			if err != nil {
-				log.Println("read:", err)
+				log.Println("Read:", err)
 				return
 			}
-			log.Printf("recv: %s", message)
+			log.Printf("Recv: %s", message)
+			var eventTxlistJson EventTxlistJson
+			err = json.NewDecoder(bytes.NewBuffer(message)).Decode(&eventTxlistJson)
+			if err != nil {
+				log.Println("Decode:", err)
+				continue
+			}
+
+			if eventTxlistJson.Event == "txlist" {
+				//没有交易记录
+				if eventTxlistJson.Result == nil {
+					continue
+				}
+
+				addr := eventTxlistJson.Address
+				startBlock,ok := eais[addr]
+				if !ok {
+					continue
+				}
+
+				maxBlock := startBlock
+
+				proc := 0
+				inc := 0
+				skip := 0
+
+				//遍历交易记录插入数据库
+				//开启事务
+				trans, err := TxBegin()
+				if err != nil {
+					log.Println("Error TxBegin:", err.Error())
+					continue
+				}
+				for _ , tx := range eventTxlistJson.Result {
+					proc++
+					err := trans.InsertTx(&tx)
+					if err != nil {
+						//插入失败显示日志
+						//txString, _ := json.Marshal(tx)
+						errString := err.Error()
+						if strings.Contains(errString, "Duplicate entry") {
+							log.Printf("Skip Duplicate tx: %s%s\n", ETHERSCANAPI_TX,tx.Hash)
+						} else {
+							log.Println("Error insertTx:", errString, tx.Hash)
+						}
+						skip++
+					}else{
+						inc++
+					}
+
+					//最后块编号
+					b, ok := strconv.Atoi(tx.BlockNumber )
+					if ok == nil {
+						if b > maxBlock {
+							maxBlock = b
+						}
+					}
+				}
+
+				//保存最后块编号
+				if maxBlock > startBlock {
+					if trans.UpdateLastBlock(addr, maxBlock) == 0 {
+						trans.Rollback()
+					}
+				}
+				trans.Commit()
+			} else if eventTxlistJson.Event == "subscribe-txlist" {
+				if eventTxlistJson.Status ==  "1" {
+					//订阅成功检索一次历史数据
+					addr := eventTxlistJson.Message[4:]
+					startBlock,ok := eais[addr]
+					if ok {
+						retrieve(addr, startBlock)
+					}
+				}
+			}
 		}
 	}()
 
 	pingTicker := time.NewTicker(time.Second*20)
 	defer pingTicker.Stop()
 
-	//订阅交易记录
-	err = c.WriteMessage(websocket.TextMessage, []byte("{\"event\": \"txlist\", \"address\": \"0x2a65aca4d5fc5b5c859090a6c34d164135398226\"}"))
+	//取得地址和最后一次更新的块号
+	eais, err = GetEthAddress()
 	if err != nil {
-		log.Println("subscribe write:", err)
-		//订阅错误输出不退出
+		log.Println("Error GetEthAddress:", err.Error())
+		return err
+	}
+
+	if len(eais) == 0 {
+		log.Println("Error not found ethereum address.")
+		return err
+	}
+	for a,_ := range(eais) {
+		//订阅交易记录
+		err = c.WriteMessage(websocket.TextMessage, []byte("{\"event\": \"txlist\", \"address\": \""+a+"\"}"))
+		if err != nil {
+			log.Println("Subscribe write:", err)
+			//订阅错误输出不退出
+		}
+
 	}
 
 	//发送Ping直到连接断开
@@ -74,17 +223,17 @@ func (e * EtherscanWS)start() error {
 		case <-pingTicker.C:
 			err := c.WriteMessage(websocket.TextMessage, []byte("{\"event\":\"ping\"}"))
 			if err != nil {
-				log.Println("ping write:", err)
+				log.Println("Ping write:", err)
 				return err
 			}
 		case <-interrupt: // 用户中断自动停止
-			log.Println("interrupt")
+			log.Println("Interrupt")
 
 			// Cleanly close the connection by sending a close message and then
 			// waiting (with timeout) for the server to close the connection.
 			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
-				log.Println("write close:", err)
+				log.Println("Write close:", err)
 				return err
 			}
 			select {
@@ -109,13 +258,100 @@ func (e * EtherscanWS)Run() {
 	}
 }
 
+
+//取回指定地址和开始块号
+func retrieve(addr string, startBlock int) {
+	proc := 0
+	inc := 0
+	skip := 0
+
+	t1 := time.Now()
+	log.Printf("Begin retrieve.(Address: %s%s, StartBlock:%d)\n", ETHERSCANAPI_ADDR, addr, startBlock)
+	defer func (){
+		elapsed := time.Since(t1)
+		log.Printf("End retrieve.(Elapsed:%v, Process:%d, Increase:%d, Skip:%d)\n", elapsed, proc, inc, skip)
+	}()
+
+	maxBlock := startBlock
+
+	//使用Etherscan API检索交易列表
+	txlistJson, err := Retrieve(addr, startBlock, true)
+	//检索失败处理
+	if err != nil {
+		log.Println("Error retrieve:", err.Error())
+		return
+	}
+
+	//API返回错误处理
+	if txlistJson.Status != "1" {
+		log.Println("Etherscan api:", txlistJson.Message)
+		return
+	}
+
+	//没有交易记录
+	if txlistJson.Result == nil {
+		return
+	}
+
+	//遍历交易记录插入数据库
+	//开启事务
+	trans, err := TxBegin()
+	if err != nil {
+		log.Println("Error TxBegin:", err.Error())
+		return
+	}
+	for _ , tx := range txlistJson.Result {
+		proc++
+		err := trans.InsertTx(&tx)
+		if err != nil {
+			//插入失败显示日志
+			//txString, _ := json.Marshal(tx)
+			errString := err.Error()
+			if strings.Contains(errString, "Duplicate entry") {
+				log.Printf("Skip Duplicate tx: %s%s\n", ETHERSCANAPI_TX,tx.Hash)
+			} else {
+				log.Println("Error insertTx:", errString, tx.Hash)
+			}
+			skip++
+		}else{
+			inc++
+		}
+
+
+		//最后块编号
+		b, ok := strconv.Atoi(tx.BlockNumber )
+		if ok == nil {
+			if b > maxBlock {
+				maxBlock = b
+			}
+		}
+	}
+
+	//保存最后块编号
+	if maxBlock > startBlock {
+		if trans.UpdateLastBlock(addr, maxBlock) == 0 {
+			trans.Rollback()
+		}
+	}
+
+	trans.Commit()
+}
+
+
 //每十分钟运行一次
 //查询最后更新块到最新块的交易记录
 //交易记录写入数据库
 
 func main() {
 	flag.Parse()
-	log.SetFlags(0)
+
+	//打开数据库
+	if err := OpenDatabase(); err != nil {
+		log.Println("Error OpenDatabase:", err.Error())
+		return
+	}
+	//程序结束前关闭数据库
+	defer CloseDatabase()
 
 	ews := NewEtherscanWS()
 	ews.Run()
